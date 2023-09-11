785b7801603519c85e00350fa4635dc6235b5440
tka4roman <tka4roman@gmail.com>
2023-09-11 21:04:14 +0300

    add new HTML line

--- app/app.py
+++ app/app.py
@@ -14,6 +14,7 @@ class MyServer(BaseHTTPRequestHandler):
         self.wfile.write(bytes("<html><head><title>Simple-dimple</title></head>", "utf-8"))
         self.wfile.write(bytes("<p>Was deployed on: %s</p>" % date, "utf-8"))
         self.wfile.write(bytes("<p>Let's go!</p>", "utf-8"))
+        self.wfile.write(bytes("<p>Autodeployed!!!!</p>", "utf-8"))
         self.wfile.write(bytes("<body>", "utf-8"))
         self.wfile.write(bytes("</body></html>", "utf-8"))
 
